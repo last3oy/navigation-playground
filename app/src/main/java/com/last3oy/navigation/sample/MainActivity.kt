22c5f43c522262f391ac58d8576b5c012794073f
last3oy <softwarez_gelion@hotmail.com>
2018-09-09 20:20:42 +0700

    trying NavigationUI by overflow menu

--- app/src/main/java/com/last3oy/navigation/sample/MainActivity.kt
+++ app/src/main/java/com/last3oy/navigation/sample/MainActivity.kt
@@ -2,11 +2,26 @@ package com.last3oy.navigation.sample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        MenuInflater(this).inflate(R.menu.menu_main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        item?.let { NavigationUI.onNavDestinationSelected(it, findNavController(R.id.nhfMain)) }
+        return super.onOptionsItemSelected(item)
     }
 }
